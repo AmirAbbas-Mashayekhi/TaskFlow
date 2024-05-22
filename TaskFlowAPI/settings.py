db957e55ba09ca113f685e8a1ca53de71751cb0a
AmirMash <AmirAbbasMashayekhi.dev@gmail.com>
2024-05-23 00:49:51 +0330

    Added Core app to INSTALLED_APPS

--- TaskFlowAPI/settings.py
+++ TaskFlowAPI/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'debug_toolbar',
     'tasks',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -60,8 +61,7 @@ ROOT_URLCONF = 'TaskFlowAPI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
