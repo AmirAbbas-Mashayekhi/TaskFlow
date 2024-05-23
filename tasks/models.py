316f1446743eac77670b88efa6a4a2e5e1f38ab3
AmirMash <AmirAbbasMashayekhi.dev@gmail.com>
2024-05-24 03:09:43 +0330

    Created Project model

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,6 @@
+import uuid
+from datetime import datetime
+
 from django.db import models
 from django.conf import settings
 from phonenumber_field.modelfields import PhoneNumberField
@@ -6,3 +9,12 @@ from phonenumber_field.modelfields import PhoneNumberField
 class Participant(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     phone = PhoneNumberField()
+
+
+class Project(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255, blank=True, null=True)
+    start_date = models.DateField(default=datetime.now)
+    end_date = models.DateField(null=True, blank=True)
+    owner = models.ForeignKey(Participant, on_delete=models.CASCADE)
