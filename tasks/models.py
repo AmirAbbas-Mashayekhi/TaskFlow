a96aa5762ff3c10799579948df953a3fd1168eb0
AmirMash <AmirAbbasMashayekhi.dev@gmail.com>
2024-05-24 03:27:31 +0330

    Created Task model

--- tasks/models.py
+++ tasks/models.py
@@ -18,3 +18,39 @@ class Project(models.Model):
     start_date = models.DateField(default=datetime.now)
     end_date = models.DateField(null=True, blank=True)
     owner = models.ForeignKey(Participant, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    PRIORITY_CRITICAL = 'C'
+    PRIORITY_HIGH = 'H'
+    PRIORITY_MEDIUM = 'M'
+    PRIORITY_LOW = 'L'
+    PRIORITY_CHOICES = [
+        (PRIORITY_CRITICAL, 'Critical'),
+        (PRIORITY_HIGH, 'High'),
+        (PRIORITY_MEDIUM, 'Medium'),
+        (PRIORITY_LOW, 'Low')
+    ]
+
+    STATUS_BACKLOG = 'B'
+    STATUS_TODO = 'T'
+    STATUS_DOING = 'D'
+    STATUS_FINISHED = 'F'
+    STATUS_ARCHIVED = 'A'
+    STATUS_CHOICES = [
+        (STATUS_BACKLOG, 'Backlog'),
+        (STATUS_TODO, 'Todo'),
+        (STATUS_DOING, 'Doing'),
+        (STATUS_FINISHED, 'Finished'),
+        (STATUS_ARCHIVED, 'Archived')
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
+    assignees = models.ManyToManyField(Participant, related_name='assignees')
+    priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default=PRIORITY_LOW)
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES, default=STATUS_BACKLOG)
+    created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField()
+    expected_duration = models.PositiveSmallIntegerField()
