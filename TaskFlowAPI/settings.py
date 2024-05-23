097c885b2d4050858f9e4b5c79fa17275b9b96f7
AmirMash <AmirAbbasMashayekhi.dev@gmail.com>
2024-05-24 02:58:53 +0330

    Created Participant model as the user profile and used PhoneNumberField for better validation.

--- TaskFlowAPI/settings.py
+++ TaskFlowAPI/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'phonenumber_field',
     'tasks',
     'core',
 ]
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.conf import settings
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+
+class Participant(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    phone = PhoneNumberField()
