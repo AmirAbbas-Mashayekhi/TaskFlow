7e9af4719d34d8259175fb9634cdb45894f28481
AmirMash <AmirAbbasMashayekhi.dev@gmail.com>
2024-05-24 04:14:29 +0330

    Implemented Simple admin panel registration

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Task, Participant, Comment, Project
 
-# Register your models here.
+
+@admin.register(Participant)
+class ParticipantAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user_name', 'first_name', 'last_name']
+    list_select_related = ['user']
+    autocomplete_fields = ['user']
+    search_fields = ['user_name__istartswith']
+    list_per_page = 10
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'project',
+        'priority',
+        'status',
+        'created_at',
+        'due_date',
+        'expected_duration'
+    ]
+    list_editable = ['priority', 'status']
+    list_per_page = 20
+    search_fields = ['title__istartswith']
+    autocomplete_fields = ['assignees', 'project']
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'posted_at', 'task']
+    autocomplete_fields = ['user', 'task']
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['title', 'owner', 'start_date', 'end_date']
+    list_select_related = ['owner']
+    autocomplete_fields = ['owner']
+    search_fields = ['title__istartswith']
--- tasks/models.py
+++ tasks/models.py
@@ -1,5 +1,6 @@
 import uuid
 from datetime import datetime
+from django.contrib import admin
 from django.db import models
 from django.conf import settings
 from phonenumber_field.modelfields import PhoneNumberField
@@ -7,16 +8,37 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 class Participant(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    phone = PhoneNumberField()
+    phone = PhoneNumberField(unique=True)
+
+    @admin.display(ordering='user__first_name')
+    def first_name(self):
+        return self.user.first_name
+
+    @admin.display(ordering='user__last_name')
+    def last_name(self):
+        return self.user.last_name
+
+    @admin.display(ordering='user__username')
+    def user_name(self):
+        return self.user.username
+
+    def __str__(self):
+        return self.user.username
+
+    class Meta:
+        ordering = ['user__username']
 
 
 class Project(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=255)
     description = models.CharField(max_length=255, blank=True, null=True)
     start_date = models.DateField(default=datetime.now)
     end_date = models.DateField(null=True, blank=True)
     owner = models.ForeignKey(Participant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
 
 
 class Task(models.Model):
@@ -45,7 +67,7 @@ class Task(models.Model):
     ]
 
     title = models.CharField(max_length=255)
-    description = models.CharField(max_length=255)
+    description = models.TextField()
     project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
     assignees = models.ManyToManyField(Participant, related_name='assignees')
     priority = models.CharField(max_length=1, choices=PRIORITY_CHOICES, default=PRIORITY_LOW)
@@ -54,9 +76,15 @@ class Task(models.Model):
     due_date = models.DateField()
     expected_duration = models.PositiveSmallIntegerField()
 
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     user = models.ForeignKey(Participant, on_delete=models.CASCADE)
     posted_at = models.DateTimeField(auto_now_add=True)
     text = models.TextField()
     task = models.ForeignKey(Task, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.task} comment'
