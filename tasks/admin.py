ad6d3ce7d01e57d70bf0cd7422f216b5868e61f9
AmirMash <AmirAbbasMashayekhi.dev@gmail.com>
2024-05-26 02:38:57 +0330

    Tuned Admin panel functionality to the new models.

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Task, Participant, Comment, Project
+
+from .models import Task, Participant, Comment, Project, Team
 
 
 @admin.register(Participant)
@@ -7,7 +8,7 @@ class ParticipantAdmin(admin.ModelAdmin):
     list_display = ['id', 'user_name', 'first_name', 'last_name']
     list_select_related = ['user']
     autocomplete_fields = ['user']
-    search_fields = ['user_name__istartswith']
+    search_fields = ['user__username__istartswith']
     list_per_page = 10
 
 
@@ -25,19 +26,32 @@ class TaskAdmin(admin.ModelAdmin):
     ]
     list_editable = ['priority', 'status']
     list_per_page = 20
-    search_fields = ['title__istartswith']
+    search_fields = ['title']
     autocomplete_fields = ['assignees', 'project']
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['title', 'leader', 'members_count']
+    search_fields = ['title']
+    autocomplete_fields = ['members', 'leader']
+    list_per_page = 10
+
+    @staticmethod
+    def members_count(team):
+        return team.members.count()
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['id', 'user', 'posted_at', 'task']
     autocomplete_fields = ['user', 'task']
+    list_per_page = 10
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ['title', 'owner', 'start_date', 'end_date']
-    list_select_related = ['owner__user']
-    autocomplete_fields = ['owner']
+    list_display = ['title', 'team', 'start_date', 'end_date']
+    autocomplete_fields = ['team']
     search_fields = ['title__istartswith']
+    list_per_page = 10
