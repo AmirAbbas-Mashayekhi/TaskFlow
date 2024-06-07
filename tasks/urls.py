e4c48ef07c4ae30db0db660aa2693c2654c69efd
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-06-08 00:41:06 +0330

    Registered Route for ProjectViewSet.

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from rest_framework_nested import routers
-from .views import InvitationViewSet, ParticipantViewSet, TeamMemberViewSet, TeamViewSet
+from .views import (
+    InvitationViewSet,
+    ParticipantViewSet,
+    ProjectViewSet,
+    TeamMemberViewSet,
+    TeamViewSet,
+)
 
 router = routers.DefaultRouter()
 
 router.register("participants", ParticipantViewSet, basename="participants")
 router.register("teams", TeamViewSet, basename="teams")
 router.register("invitations", InvitationViewSet, basename="invitations")
+router.register("projects", ProjectViewSet, basename="projects")
 
 teams_router = routers.NestedDefaultRouter(router, "teams", lookup="teams")
 teams_router.register("members", TeamMemberViewSet, basename="members")
